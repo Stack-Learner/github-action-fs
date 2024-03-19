17568869e18c8690310bc2a6df5875c971ecc09d
HM Nayem <hasan.m.nayem@gmail.com>
2024-03-20 00:52:35 +0600

    feat: added delivery workflow

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.status(201).json({ message: 'Hello World!' });
+	res.send('Hello World!');
 });
 
 module.exports = app;
